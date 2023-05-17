75b2b86a59157ea77806fc871295c02e89473637
Samuel James <64286580+Samuel-James2003@users.noreply.github.com>
2023-05-17 10:07:44 +0200

    Test

--- KnuckleBones/GameScreen.cs
+++ KnuckleBones/GameScreen.cs
@@ -35,8 +35,7 @@ namespace KnuckleBones
             pen = new Pen(Color.Black, 1);
             InitializeComponent();
             player1 = new Player(col, row);
-            player2 = new Player(col, row);
-            player2.Offset = offset;
+            player2 = new Player(col, row, offset);
             players.Add(player1);
             players.Add(player2);
         }
